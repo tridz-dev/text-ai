953057344098372de89d75b14d45b34e2c7da553
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2023-03-09 12:34:56 +0530

    removed explain

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -66,14 +66,14 @@ function Content() {
                     <path d="M6 8.6l-2 -1.1"></path>
                   </svg>
                   <span>{t("Suggest")}</span></Link>
-                <Link to="/explain"
+                {/* <Link to="/explain"
                 className={outlineButtonCss}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-school mr-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
                     <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
                   </svg>
-                  <span>{t("explain")}</span></Link>
+                  <span>{t("explain")}</span></Link> */}
                 <Link to="/imagine"
                 className={outlineButtonCss}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-crystal-ball mr-2" width="20" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
