fa70393ebba1d2829eaf18d5f1e0e070df724379
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2023-03-17 17:47:11 +0530

    fixed sizes in arabic

--- src/Layouts/Dashboard/Sidebar/Sidebar.jsx
+++ src/Layouts/Dashboard/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { NavVertical } from "./../Nav";
 function Sidebar() {
   return (
-    <aside className="w-[200px] flex-col md:flex hidden bg-nav-blue h-screen pt-4 px-6 shadow-inner">
+    <aside className="w-[250px] flex-col md:flex hidden bg-nav-blue h-screen pt-4 px-6 shadow-inner">
       <span className="hidden font-bold text-2xl sm:inline-block mb-3 mx-3">Imagine</span>
       <NavVertical />
     </aside>
