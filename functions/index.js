cc4f8ae027d295634aff03efd954a95c9ff7dadd
esafwan <esafwan@gmail.com>
2023-03-19 18:02:48 +0530

    Added functions.

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+
+
+const api = require("./api");
+const test = require("./test");
+
+exports.api = api;
+exports.test = test;
